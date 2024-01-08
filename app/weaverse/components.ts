149fa45b1cb23bfb2cd8c75d0d3629381249b8a9
dangthang1903 <dangthangkk2000@gmail.com>
2024-01-08 09:29:22 +0700

    feat: add header image section

--- app/weaverse/components.ts
+++ app/weaverse/components.ts
@@ -1,4 +1,8 @@
 import type {HydrogenComponent} from '@weaverse/hydrogen';
-import * as WeaverseButton from '~/sections/atoms/button';
 import * as RecommendedProducts from '~/sections/recommended-products';
-export const components: HydrogenComponent[] = [WeaverseButton];
+import * as HeaderImage from '~/sections/HeaderImage/HeaderImage';
+import { atoms } from '~/sections/atoms/atoms';
+export const components: HydrogenComponent[] = [
+    ...atoms,
+    HeaderImage,
+];
