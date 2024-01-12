e0982793ad06d4bc5d703eb0a159834743835967
dangthang1903 <dangthangkk2000@gmail.com>
2024-01-12 17:29:59 +0700

    chore: refactor

--- app/weaverse/components.ts
+++ app/weaverse/components.ts
@@ -1,4 +1,4 @@
-import type {HydrogenComponent} from '@weaverse/hydrogen';
+import type { HydrogenComponent } from '@weaverse/hydrogen';
 import * as RecommendedProducts from '~/sections/recommended-products';
 import * as StyleGuide from '~/sections/style-guide';
 import * as HeaderImage from '~/sections/Image-banner/Image-banner';
@@ -6,4 +6,5 @@ import { atoms } from '~/sections/atoms/atoms';
 export const components: HydrogenComponent[] = [
     ...atoms,
     HeaderImage,
-, StyleGuide];
+    StyleGuide
+];
