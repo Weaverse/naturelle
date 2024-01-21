7fcb14da45d9acb92179e329781794feeb152174
dangthang1903 <dangthangkk2000@gmail.com>
2024-01-21 10:23:30 +0700

    refactor: update video banner section

--- app/weaverse/components.ts
+++ app/weaverse/components.ts
@@ -1,14 +1,14 @@
 import type { HydrogenComponent } from '@weaverse/hydrogen';
 import * as RecommendedProducts from '~/sections/recommended-products';
 import * as StyleGuide from '~/sections/style-guide';
-import * as HeaderImage from '~/sections/Image-banner/Image-banner';
+import * as VideoBanner from '~/sections/video-banner';
 import * as Highlights from '~/sections/hightlight/index';
 import * as HightlightItem from '~/sections/hightlight/item';
 import * as ImageWithText from '~/sections/image-with-text/index';
 import { atoms } from '~/sections/atoms/atoms';
 export const components: HydrogenComponent[] = [
     ...atoms,
-    HeaderImage,
+    VideoBanner,
     Highlights,
     HightlightItem,
     ImageWithText,
