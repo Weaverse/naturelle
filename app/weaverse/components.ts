3a16b1c6823e1cc1fceec29a57dc728507c739f2
dangthang1903 <dangthangkk2000@gmail.com>
2024-01-22 16:31:27 +0700

    feat: add testimonial section

--- app/weaverse/components.ts
+++ app/weaverse/components.ts
@@ -5,6 +5,8 @@ import * as HeaderImage from '~/sections/Image-banner/Image-banner';
 import * as Highlights from '~/sections/hightlight/index';
 import * as HightlightItem from '~/sections/hightlight/item';
 import * as ImageWithText from '~/sections/image-with-text/index';
+import * as Testimonials from '~/sections/testimonials/index';
+import * as Review from '~/sections/testimonials/review';
 import { atoms } from '~/sections/atoms/atoms';
 export const components: HydrogenComponent[] = [
     ...atoms,
@@ -12,5 +14,7 @@ export const components: HydrogenComponent[] = [
     Highlights,
     HightlightItem,
     ImageWithText,
+    Testimonials,
+    Review,
     StyleGuide
 ];
