3f888408d5a03bd7f3c44bf1451b85fb50e4ac5f
ken <viethungpro97@gmail.com>
2024-04-11 09:59:57 +0700

    chore: remove duplicated code

--- app/components/Icon.tsx
+++ app/components/Icon.tsx
@@ -304,28 +304,6 @@ export function IconHalfFilledStar(props: IconProps) {
   );
 }
 
-export function IconClose(props: IconProps) {
-  return (
-    <Icon {...props} stroke={props.stroke || 'currentColor'}>
-      <title>Close</title>
-      <line
-        x1="4.44194"
-        y1="4.30806"
-        x2="15.7556"
-        y2="15.6218"
-        strokeWidth="1.25"
-      />
-      <line
-        y1="-0.625"
-        x2="16"
-        y2="-0.625"
-        transform="matrix(-0.707107 0.707107 0.707107 0.707107 16 4.75)"
-        strokeWidth="1.25"
-      />
-    </Icon>
-  );
-}
-
 export function IconFilters(props: IconProps) {
   return (
     <Icon {...props} fill="transparent" stroke={props.stroke || 'currentColor'}>
