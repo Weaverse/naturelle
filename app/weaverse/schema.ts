e6cf68d5dec36db00db29b151cdd77ab528fa7c2
dangthang1903 <dangthangkk2000@gmail.com>
2024-07-04 17:42:53 +0700

    chore: update theme setting

--- app/weaverse/schema.ts
+++ app/weaverse/schema.ts
@@ -29,8 +29,26 @@ export const themeSchema: HydrogenThemeSchema = {
       ],
     },
     {
-      group: 'Logo',
+      group: 'Header',
       inputs: [
+        {
+          type: "toggle-group",
+          label: "Header menu type",
+          name: "typeMenuHeader",
+          configs: {
+            options: [
+              { value: "mega", label: "Mega" },
+              { value: "drawer", label: "Drawer" },
+            ]
+          },
+          defaultValue: "mega"
+        },
+        {
+          type: "switch",
+          label: "Enable transparent header",
+          name: "enableTransparentHeader",
+          defaultValue: true,
+        },
         {
           type: 'image',
           name: 'logoData',
@@ -38,6 +56,17 @@ export const themeSchema: HydrogenThemeSchema = {
           defaultValue: {
             altText: 'Logo',
             url: 'https://cdn.shopify.com/s/files/1/0838/0052/3057/files/naturelle_logo.png?v=1705045487',
+            width: 320,
+            height: 116,
+          },
+        },
+        {
+          type: 'image',
+          name: 'transparentLogoData',
+          label: 'Trasparent Logo',
+          defaultValue: {
+            altText: 'Logo',
+            url: 'https://cdn.shopify.com/s/files/1/0652/5888/1081/files/Property_1_White.png?v=1720064102',
             width: 320,
             height: 116,
           },
@@ -246,6 +275,18 @@ export const themeSchema: HydrogenThemeSchema = {
       group: 'Layout',
       inputs: [
         {
+          type: "range",
+          label: "Page width",
+          name: "pageWidth",
+          configs: {
+            min: 1000,
+            max: 1600,
+            step: 10,
+            unit: "px",
+          },
+          defaultValue: 1280,
+        },
+        {
           type: 'range',
           label: 'Nav height (mobile)',
           name: 'navHeightMobile',
@@ -256,6 +297,30 @@ export const themeSchema: HydrogenThemeSchema = {
             unit: 'rem',
           },
           defaultValue: 3,
+        },
+        {
+          type: "range",
+          label: "Nav height (tablet)",
+          name: "navHeightTablet",
+          configs: {
+            min: 2,
+            max: 8,
+            step: 1,
+            unit: "rem",
+          },
+          defaultValue: 4,
+        },
+        {
+          type: "range",
+          label: "Nav height (desktop)",
+          name: "navHeightDesktop",
+          configs: {
+            min: 2,
+            max: 8,
+            step: 1,
+            unit: "rem",
+          },
+          defaultValue: 6,
         },
       ],
     },
