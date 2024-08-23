538e79b1591225fe4248138c617ac55691f283cd
dangthang1903 <dangthangkk2000@gmail.com>
2024-08-23 17:31:48 +0700

    refactor collection filter

--- app/sections/collection-filters/index.tsx
+++ app/sections/collection-filters/index.tsx
@@ -4,20 +4,53 @@ import type {Filter} from '@shopify/hydrogen/storefront-api-types';
 import type {
   HydrogenComponentSchema,
 } from '@weaverse/hydrogen';
-import {Button} from '~/components/Button';
 import {DrawerFilter} from '~/components/DrawerFilter';
 import type {AppliedFilter} from '~/lib/filter';
 import {forwardRef} from 'react';
 import {useInView} from 'react-intersection-observer';
 import type {CollectionDetailsQuery} from 'storefrontapi.generated';
-import {layoutInputs, Section, SectionProps} from '../atoms/Section';
 import {ProductsLoadedOnScroll} from './products-loaded-on-scroll';
+import { Button } from '@/components/button';
+import { cva, VariantProps } from 'class-variance-authority';
+import { cn } from '@/lib/utils';
 
-type CollectionFiltersProps = SectionProps;
+interface CollectionFiltersProps extends VariantProps<typeof variants> {};
+let variants = cva("relative lg:pb-20 pb-12", {
+  variants: {
+    width: {
+      full: "w-full h-full",
+      stretch: "w-full h-full",
+      fixed: "w-full h-full max-w-[var(--page-width,1440px)] mx-auto",
+    },
+    padding: {
+      full: "",
+      stretch: "px-3 md:px-10 lg:px-16",
+      fixed: "px-3 md:px-4 lg:px-6 mx-auto",
+    },
+    gap: {
+      0: "",
+      4: "space-y-1",
+      8: "space-y-2",
+      12: "space-y-3",
+      16: "space-y-4",
+      20: "space-y-5",
+      24: "space-y-3 lg:space-y-6",
+      28: "space-y-3.5 lg:space-y-7",
+      32: "space-y-4 lg:space-y-8",
+      36: "space-y-4 lg:space-y-9",
+      40: "space-y-5 lg:space-y-10",
+      44: "space-y-5 lg:space-y-11",
+      48: "space-y-6 lg:space-y-12",
+      52: "space-y-6 lg:space-y-[52px]",
+      56: "space-y-7 lg:space-y-14",
+      60: "space-y-7 lg:space-y-[60px]",
+    },
+  },
+});
 
 let CollectionFilters = forwardRef<HTMLElement, CollectionFiltersProps>(
   (props, sectionRef) => {
-    let {children, ...rest} = props;
+    let {width, gap, ...rest} = props;
     let {ref, inView} = useInView();
     let {collection, collections, appliedFilters} = useLoaderData<
       CollectionDetailsQuery & {
@@ -30,7 +63,7 @@ let CollectionFilters = forwardRef<HTMLElement, CollectionFiltersProps>(
 
     if (collection?.products && collections) {
       return (
-        <Section ref={sectionRef} {...rest}>
+        <section ref={sectionRef} {...rest} className={cn(variants({ gap, width, padding: width }),)}>
           <DrawerFilter
             productNumber={productNumber}
             filters={collection.products.filters as Filter[]}
@@ -48,9 +81,9 @@ let CollectionFilters = forwardRef<HTMLElement, CollectionFiltersProps>(
               state,
             }) => (
               <>
-                <div className="my-6 flex w-full items-center justify-center">
-                  <Button as={PreviousLink} variant="secondary">
-                    {isLoading ? 'Loading...' : 'Load previous'}
+                <div className="!mb-6 flex w-full items-center justify-center">
+                  <Button as={PreviousLink} variant="outline">
+                    <span className='font-heading font-light'>{isLoading ? 'Loading...' : 'Load previous'}</span>
                   </Button>
                 </div>
                 <ProductsLoadedOnScroll
@@ -60,15 +93,15 @@ let CollectionFilters = forwardRef<HTMLElement, CollectionFiltersProps>(
                   hasNextPage={hasNextPage}
                   state={state}
                 />
-                <div className="my-6 flex w-full items-center justify-center">
-                  <Button as={NextLink} variant="secondary">
-                    {isLoading ? 'Loading...' : 'Load more products'}
+                <div className="!mt-14 flex w-full items-center justify-center">
+                  <Button as={NextLink} variant="outline">
+                    <span className='font-heading font-light my-0.5'>{isLoading ? 'Loading...' : 'Show more +'}</span>
                   </Button>
                 </div>
               </>
             )}
           </Pagination>
-        </Section>
+        </section>
       );
     }
     return <section ref={ref} {...rest} />;
@@ -88,9 +121,33 @@ export let schema: HydrogenComponentSchema = {
   inspector: [
     {
       group: 'Collection filters',
-      inputs: layoutInputs.filter(
-        ({name}) => name !== 'divider' && name !== 'borderRadius',
-      ),
+      inputs: [
+        {
+          type: "select",
+          name: "width",
+          label: "Content width",
+          configs: {
+            options: [
+              { value: "full", label: "Full page" },
+              { value: "stretch", label: "Stretch" },
+              { value: "fixed", label: "Fixed" },
+            ],
+          },
+          defaultValue: "fixed",
+        },
+        {
+          type: "range",
+          name: "gap",
+          label: "Items spacing",
+          configs: {
+            min: 0,
+            max: 60,
+            step: 4,
+            unit: "px",
+          },
+          defaultValue: 20,
+        },
+      ]
     },
   ],
 };
