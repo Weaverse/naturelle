1a44f1c162334432b2327879604db9b0a1117ff2
dangthang1903 <dangthangkk2000@gmail.com>
2024-10-04 10:23:02 +0700

    update slide section

--- app/sections/slides/slide.tsx
+++ app/sections/slides/slide.tsx
@@ -71,14 +71,14 @@ const Slide = forwardRef<HTMLDivElement, SlideProps>((props, ref) => {
                             </div>
                         )}
                     </div>
-                    <div className='relative flex items-center px-6 py-12 bg-[var(--background-color)] aspect-square w-full h-1/2 sm:w-1/2 sm:h-full sm:px-14 sm:py-20'>
+                    <div className='flex flex-col gap-0 md:gap-6 lg:gap-20 items-center justify-center px-6 py-12 bg-[var(--background-color)] aspect-square w-full h-1/2 sm:w-1/2 sm:h-full sm:px-14 sm:py-20'>
                         <div className={clsx(
                             'flex flex-col justify-center gap-4',
                             alignmentClasses[textAlignment!],
                         )}>
                             {children}
                         </div>
-                        <div className='flex gap-4 justify-center absolute bottom-12 left-1/2 -translate-x-1/2'>
+                        <div className='sm:flex gap-4 justify-center items-center hidden'>
                             <IconArrowLeft onClick={() => swiper.slidePrev()} className='w-8 h-8 cursor-pointer' viewBox='0 0 32 32' />
                             <IconArrowRight onClick={() => swiper.slideNext()} className='w-8 h-8 cursor-pointer' viewBox='0 0 32 32' />
                         </div>
