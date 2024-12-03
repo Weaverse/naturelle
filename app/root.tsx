69bfddc7d7635fb5e3c12333cf880d30512920d8
dangthang <dangthangkk2000@gmail.com>
2024-12-03 16:03:50 +0700

    refactor: adjust ErrorBoundary layout for better responsiveness and structure in root.tsx; updated height and flex properties

--- app/root.tsx
+++ app/root.tsx
@@ -209,7 +209,7 @@ export function ErrorBoundary() {
   }
 
   return (
-    <div className="relative flex h-80 w-full items-center justify-center md:h-[500px] lg:h-[720px]">
+    <div className="relative flex w-full items-center justify-center h-screen">
       <div className="absolute inset-0 h-full w-full">
         <Image
           src="https://cdn.shopify.com/s/files/1/0652/5888/1081/files/d63681d5f3e2ce453bcac09ffead4d62.jpg?v=1720369103"
