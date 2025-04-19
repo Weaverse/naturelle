6084ab4d22b9718f4666350644e7f1b14af186a2
Paul <phanminh65@gmail.com>
2025-04-19 14:08:27 +0700

    chore: update package dependencies in package.json and package-lock.json to latest versions, including @remix-run and @shopify packages; add v3_lazyRouteDiscovery option in vite.config.ts; adjust stylesheet link in root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -63,7 +63,6 @@ export let shouldRevalidate: ShouldRevalidateFunction = ({
 
 export function links() {
   return [
-    { rel: "stylesheet", href: tailwind },
     {
       rel: "preconnect",
       href: "https://cdn.shopify.com",
@@ -156,6 +155,7 @@ export function Layout({ children }: { children?: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <link rel="stylesheet" href={tailwind} />
         <Meta />
         <Links />
         <GlobalStyle />
